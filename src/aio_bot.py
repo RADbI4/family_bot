50bedd070c4ee569a882f48f560c1382f7835a24
Radmir <capitanpit@yandex.ru>
2023-12-03 13:25:18 +0300

    clening

--- src/aio_bot.py
+++ src/aio_bot.py
@@ -1,13 +1,8 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from .states import YaDriveStates
-from .publishers import yandex_drive_publish, calendar_in_publish
-from base64 import b64encode
-import datetime
 import os
-import json
-
+from time import sleep
 storage = MemoryStorage()
 family_bot = Bot(token=os.environ.get('telegram_bot_1'))
 dp = Dispatcher(family_bot, storage=storage)
@@ -18,65 +13,22 @@ def get_start_msgs(message):
     family_bot.send_message(message.chat.id, text="Меня зовут radmir_family_bot, я помошник вашей семьи.\n"
                                                   "Наберите /help, чтобы узнать о моих возможностях.")
 
-
-@dp.message_handler(commands=['files'])
-async def files_call(message):
-    """
-    Транспорт файлов на загрузку в yandex cloud.
-    :param message:
-    :return:
-    """
-    text = 'Окей, Загрузите ваши файлы'
-    await YaDriveStates.wait_for_files.set()
-    await family_bot.send_message(message.chat.id, text=text)
-
-@dp.message_handler(content_types=['video'], state=YaDriveStates.wait_for_files)
-async def files_callback(message, state: FSMContext):
-    text = f'Файлы загружаются на Яндекс диск'
-    # file_name = message.photo[-1].file_unique_id
-    id =message.message_id
-    pass
-
-@dp.message_handler(content_types=['photo'], state=YaDriveStates.wait_for_files)
-async def files_callback(message, state: FSMContext):
-    text = f'Файлы загружаются на Яндекс диск'
-    # file_name = message.photo[-1].file_unique_id
-    id =message.message_id
-    pass
-    # file_id_info = await family_bot.get_file(message.photo[-1].file_id)
-    # downloaded_file = await family_bot.download_file(file_path=file_id_info.file_path)
-    # file_to_floader = b64encode(downloaded_file.getvalue()).decode('utf-8')
-    # data = {'f_data': file_to_floader,
-            # 'f_name': file_name + '.jpg',
-            # 'action': 'load_files',
-            # 'folder_name': datetime.datetime.date(datetime.datetime.today()).strftime('%d-%m-%y')}
-    # rabbitMQ_body = json.dumps(data)
-    # yandex_drive_publish(msg=rabbitMQ_body)
-
-    await family_bot.send_message(message.chat.id, text=text, reply_markup=types.ReplyKeyboardRemove())
-    await state.finish()
-
-
 @dp.message_handler(commands=['help'])
 async def get_help_msgs(message):
     await family_bot.send_message(message.chat.id, text="Я могу сделать для вас следующее:\n"
                                                         "1. Чтобы узнать, когда у Радмира выходной, напишите- /work \n"
-                                                        "2. Чтобы сохранить ваши фото на Яндекс диск Камиллы и Радмира\n"
-                                                        "нажмите /files\n")
+                                                        )
 
 @dp.message_handler(commands=['work'])
 async def answer_work(message: types.Message, state: FSMContext):
     """
-    Отправляет сообщение в очередь RabbitMQ work_d_out
+    Отвечает за отправку ссыли на моё рассписание
     :param call:
     :return:
     """
-    async with state.proxy() as data:
-        data['name'] = message.text
-    rabbitMQ_body = json.dumps({'load': True})
-    calendar_in_publish(msg=rabbitMQ_body)
-    await family_bot.send_message(message.chat.id, text='Скоро будет загружен календарь\n '
-                                                        'рабочих дней Радмира!')
+    sleep(3)
+    await family_bot.send_message(message.chat.id, text='Календарь рабочих дней радмира:\n'
+                                                        'https://docs.google.com/spreadsheets/d/1_Iuwdc1ytRH8yiFITAG1Pf_Y1BTe9m5o57aDPkSqRW4/edit#gid=1462589716')
 
 if __name__ == '__main__':
     # executor.start_polling(dp)
--- src/aio_keyboards.py
+++ src/aio_keyboards.py
@@ -1,16 +1,5 @@
 from aiogram import types
 
-
-class CalendarKeyboards:
-    @staticmethod
-    def calendar_kb():
-        keyboard = types.ReplyKeyboardMarkup()
-        keyboard.add(types.KeyboardButton(text="Сегодня"))
-        keyboard.add(types.KeyboardButton(text="Завтра"))
-        keyboard.add(types.KeyboardButton(text="Послезавтра"))
-        return keyboard
-
-
 class YaKeyboards:
     @staticmethod
     def drive_kb():
